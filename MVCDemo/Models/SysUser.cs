1314d77087b30a4db63032363e6b03dd3122a76c
Spartan <bingwangzhao@163.com>
2016-05-24 22:44:10 +0800

    003

---
+++ MVCDemo/Models/SysUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDemo.Models
+{
+    public class SysUser
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public virtual ICollection<SysUserRole> SysUserRoles { get; set; }
+    }
+}
---
+++ MVCDemo/Models/SysUserRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDemo.Models
+{
+    public class SysUserRole
+    {
+        public int ID { get; set; }
+        public int SysUserID { get; set; }
+        public int SysRoleID { get; set; }
+        public virtual SysUser SysUser { get; set; }
+        public virtual SysRole SysRole { get; set; }
+    }
+}
