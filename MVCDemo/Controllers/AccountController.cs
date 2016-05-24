11c9c11fa8e477b82f2e41783d69b0b53c98f822
Spartan <bingwangzhao@163.com>
2016-05-24 13:49:38 +0800

    first

---
+++ MVCDemo/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.LoginState = "登录前......";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            string email = fc["inputEmail"];
+            string password = fc["inputPassword"];
+            ViewBag.LoginState = email + "登陆后......";
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
